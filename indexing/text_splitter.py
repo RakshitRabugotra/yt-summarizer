836749bb067ea59cad3641975f03e09a900aaec1
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2025-07-13 15:46:45 +0530

    fix: add correct types, docstrings

--- indexing/text_splitter.py
+++ indexing/text_splitter.py
@@ -1,5 +1,5 @@
 import os
-from typing import Iterable, TypedDict
+from typing import Iterator, TypedDict
 from langchain.schema import Document
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -8,7 +8,7 @@ from langchain_core.runnables import RunnableLambda
 # Custom modules
 from indexing.vectorstore import get_vector_store
 
-CHUNK_SIZE=1000
+CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 0.20 * CHUNK_SIZE
 
 # Perform a check if we don't have OpenAI Key, then trow error
@@ -18,29 +18,47 @@ if not os.getenv("OPENAI_API_KEY"):
 """
 Formats the documents into strings
 """
+
+
 class FormatDocumentsInputs(TypedDict):
-    chunks: list[Document]
+    chunks: Iterator[Document]
     query: str
+
 
 class FormatDocumentOutputs(TypedDict):
     context: str
     query: str
 
-def __format_documents(inputs: FormatDocumentsInputs):
-    formatted_text = "\n\n".join(chunk.page_content for chunk in inputs['chunks'])
-    inputs['context'] = formatted_text
+
+def __format_documents(inputs: FormatDocumentsInputs) -> FormatDocumentOutputs:
+    """
+    Format the chunks of documents into a string with paragraph formatting
+    Args:
+        inputs: { chunks: Iterator[Document], query: str }
+    Returns:
+        outputs: { context: str, query: str }
+    """
+    # Print for the debug
+    print(f"[DEBUG]: Formatting chunks into context...")
+
+    formatted_text = "\n\n".join(chunk.page_content for chunk in inputs["chunks"])
+    inputs["context"] = formatted_text
     return inputs
+
 
 runnable_format_documents = RunnableLambda(__format_documents)
 
 """
 Splits the documents and returns embedding vectors
 """
+
+
 # The required inputs for this chain is `documents`
 class SplitEmbedAndStoreInputs(TypedDict):
     query: str
-    docs: Iterable[Document]
+    docs: Iterator[Document]
     video_url: str
+
 
 # The outputs for this class would be `chunks` and `embedding function`
 class SplitEmbedAndStoreOutput(TypedDict):
@@ -48,19 +66,29 @@ class SplitEmbedAndStoreOutput(TypedDict):
     video_url: str
 
 
-def __split_embed_and_store(inputs: SplitEmbedAndStoreInputs) -> SplitEmbedAndStoreOutput:
+def __split_embed_and_store(
+    inputs: SplitEmbedAndStoreInputs,
+) -> SplitEmbedAndStoreOutput:
     """
-    Splits the documents and returns embedding vectors
+    Splits the documents, embeds them and stores them to the vector store
+    Args:
+        inputs: { query: str, docs: Iterator[Document], video_url: str }
+    Returns:
+        output: { query: str, video_url: str }
     """
-    splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    # Print for a debug statement
+    print(f"[DEBUG]: Creating chunks and storing...")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP
+    )
     # Split the documents
-    chunks = splitter.split_documents(inputs['docs'])
+    chunks = splitter.split_documents(inputs["docs"])
     # Get the vector store
     vectorstore = get_vector_store()
     # Add the chunks to the vector store
     vectorstore.add_documents(chunks)
     return inputs
 
+
 runnable_split_embed_and_store = RunnableLambda(__split_embed_and_store)
-
-
--- retrieval/retriever.py
+++ retrieval/retriever.py
@@ -1,6 +1,10 @@
 from typing import TypedDict
-from langchain_core.runnables import RunnableLambda
 
+# Langchain imports
+from langchain_core.runnables import RunnableLambda
+from langchain.schema import Document
+
+# Custom imports
 from indexing.vectorstore import get_vector_store
 from indexing.document_loader import YouTubeTranscriptsLoader
 
@@ -11,27 +15,31 @@ class RetrievalInputs(TypedDict):
 
 
 class RetrievalOutputs(TypedDict):
-    chunks: str
+    chunks: list[Document]
     query: str
     video_url: str
 
 
-def __retrieve_docs(inputs: RetrievalInputs):
+def __retrieve_docs(inputs: RetrievalInputs) -> RetrievalOutputs:
+    """
+    Retrieves the documents for given input
+    Args:
+        inputs: { query: str, video_url: str }
 
+    Returns:
+        outputs: { chunks: list[Document], query: str, video_url: str}
+    """
+    print(f"[DEBUG]: Retrieving documents for video='{inputs['video_url']}'")
     # Get the video_id
-    video_id = YouTubeTranscriptsLoader.get_video_id(inputs['video_url'])
-
+    video_id = YouTubeTranscriptsLoader.get_video_id(inputs["video_url"])
     # Get the retriever
     retriever = get_vector_store().as_retriever(
         search_type="similarity",
         search_kwargs={"k": 4, "filter": {"video_id": video_id}},
     )
-
     # Append matching chunks to the output
-    inputs['chunks'] = retriever.invoke(inputs['query'])
-
+    inputs["chunks"] = retriever.invoke(inputs["query"])
     return inputs
-
 
 
 # The main export from this module
