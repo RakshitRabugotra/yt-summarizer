1628d3eb9f4cc00e98a454393038a71479844743
Rakshit Rabugotra <rakshit.rabugotra000@gmail.com>
2025-07-13 15:47:03 +0530

    fix: adjust for new architecture

--- main.py
+++ main.py
@@ -1,46 +1,63 @@
-# Load all the env variables
-from dotenv import load_dotenv
-load_dotenv()
+"""
+The Streamlit client for our interaction
+"""
+import streamlit as st
+from retrieval_chain import get_summary_results
 
-# Then load all the modules
-from indexing.document_loader import load_documents
-from indexing.text_splitter import split_and_embed
-from indexing.vectorstore import add_docs_to_vector_store
+#
+# Utility function
+#
+def invoke_retrieval_chain():
+    # Get the input from input box
+    video_url = st.session_state.video_url
+    query = st.session_state.query
 
-from retrieval.retriever import get_retrieved_docs
-
-from augmentation.augment_query import augmented_prompt
-
-from generation.llm import generate
-
-
-def main():
-    # Ask the user for youtube urls or ids
-    vids = input("Enter the URLs of YT Video you want to summarize (Separate by comma, if multiple)\n: ")
-
-    if vids.count(",") >= 1:
-        vids = [vid.strip() for vid in vids.split(",")]
-    else:
-        vids = [vids.strip()]
-
-    # Also ask the question that the user want to ask
-    query = input("\nGreat, now enter your query!\n: ").strip()
-
-    # We will load the documents, chunk them, embed them, and store them to vector
-    # After that, we can safely generate a retriever and retrieve the documents 
-    rag_chain = load_documents | split_and_embed | add_docs_to_vector_store | get_retrieved_docs | augmented_prompt | generate
-
-    # Invoking this we will get the resultant vectorstore
-    output = rag_chain.invoke({
-        'query': query,
-        'yt_video_urls': vids,
+    # Call the backend chain
+    success, response = get_summary_results({
+        "query": query,
+        "video_url": video_url
     })
 
-    filename = './out/response.md'
-    with open(filename, mode='w+', encoding='utf-16') as file:
-        file.write(output)
-    print("Output is written to the file: " + filename)
+    # Set session state accordingly
+    if success:
+        st.session_state.model_output = response
+        if st.session_state.get('model_error'): del st.session_state['model_error']
+    else:
+        st.session_state.model_error = response
+        if st.session_state.get('model_output'): del st.session_state['model_output']
 
+#
+# UI Elements
+#
 
-if __name__ == '__main__':
-    main()
+# Input for video URL
+st.text_input(
+    label="Video URL",
+    placeholder="https://www.youtube.com/watch?v=4g-fPNjizrw",
+    key="video_url",
+)
+
+# Input for user query
+st.text_input(
+    label="Additional Requests",
+    placeholder="You can add additional requests here...",
+    key="query"
+)
+
+# Summarize button
+st.button(
+    label="Let's summarize",
+    key="generate_button",
+    on_click=invoke_retrieval_chain,
+    disabled=not bool(st.session_state.video_url),
+)
+
+# Error Display
+if "model_error" in st.session_state:
+    st.error("### Error while generating response")
+    st.error(st.session_state.model_error)
+
+# Output Display
+if "model_output" in st.session_state:
+    st.markdown("### Here is what the video says")
+    st.markdown(st.session_state.model_output)
